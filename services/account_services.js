9ece08332ee54a4dc15eb916d243b09266095286
KienPT <phamkien.8a@gmail.com>
2024-06-17 15:32:52 +0700

    refactor(checkEmailValid): change email validation regex

--- services/account_services.js
+++ services/account_services.js
@@ -64,7 +64,7 @@ function validateLoginInputs(email, password) {
  * @returns {boolean} 
  */
 function checkEmailValid(email) {
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const emailRegex = /^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$/;
 	return emailRegex.test(email);
 }
 
