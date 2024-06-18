fe36f1f6a50512004fb27ac96e87544b0d7cfe0c
KienPT <phamkien.8a@gmail.com>
2024-06-18 11:08:39 +0700

    docs(staff controller): add doc for login route

--- routes/staffs.js
+++ routes/staffs.js
@@ -4,6 +4,82 @@ const router = express.Router();
 const staffController = require("../controllers/staff_controller");
 // const { } = require("../middlewares/auth");
 
+/**
+ * @swagger
+ * /staffs/login:
+ *   post:
+ *     summary: User login with own account
+ *     tags: [Staff]
+ *     description: Authenticate and Log user in with the provided account
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: john.doe@example.com
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 example: P@ssw0rd
+ *     responses:
+ *       200:
+ *         description: Logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Logged in successfully
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                       example: staff
+ *                     token: 
+ *                       type: string
+ *                       example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNzE4NjgyNjU2fQ.d0bcKUIeZUeOv05E0yYz_cqMEkLi16zIzsuGo8HvnQw
+ *       400:
+ *         description: Client error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: Error message
+ *       500:
+ *         description: Error when executing query
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: Database query error
+ *                 error:
+ *                   type: string
+ *                   example: Error message
+ */
 router.post("/login", staffController.login)
 
 module.exports = router
