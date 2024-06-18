e2bd600da43275ba51b8ad2cd8dbb56285049e4b
KienPT <phamkien.8a@gmail.com>
2024-06-18 16:36:07 +0700

    feat(auth middleware): add auth middleware for check if user is staff

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,23 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const connection = require("../services/db");
+
+const tokenSecretKey = process.env.JWT_TOKEN_SECRET_KEY
 
 const isCustomer = async (req, res, next) => {
 	try {
 		const token = req.header("auth-token");
-		if (!token) 
+		if (!token)
 			return res.status(401).json({
 				success: false,
 				error: "No auth token, authorization denied!",
 			});
 		const isVerified = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
-		if (isVerified) 
+		if (isVerified)
 			return res.status(401).json({
 				success: false,
 				error: "Token authorization failed, authorization denied!",
 			});
-		
+
 		// Run a query to extract roles from db then use the result to compare
 		if (isVerified.role) {
 			return res.status(401).json({
@@ -34,4 +35,28 @@ const isCustomer = async (req, res, next) => {
 	}
 };
 
-module.exports = { isCustomer };
+const isStaff = (allowRole) => (req, res, next) => {
+	const token = req.header("auth_token");
+	if (!token)
+		return res.status(401).json({
+			success: false,
+			message: "No auth token, authorization denied!",
+		});
+	const isVerified = jwt.verify(token, tokenSecretKey);
+	if (!isVerified)
+		return res.status(401).json({
+			success: false,
+			message: "Token authorization failed, authorization denied!",
+		});
+
+	if (allowRole !== isVerified.role) {
+		return res.status(401).json({
+			success: false,
+			message: "Not authorized for this action, authorization denied!",
+		});
+	}
+	next()
+}
+
+
+module.exports = { isCustomer, isStaff };
