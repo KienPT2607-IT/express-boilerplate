a59194024fdb3fc5bb44f6f730471f946904d9b0
KienPT <phamkien.8a@gmail.com>
2024-06-26 11:45:02 +0700

    feat(view static): upload the static directory served

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/uploads')));
 
 app.use("/customers", customerRouter)
 app.use("/staffs", staffRouter)
