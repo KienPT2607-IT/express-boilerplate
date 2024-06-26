789c0ed5b7b89c033e37d3c3123b391462b50ebf
KienPT <phamkien.8a@gmail.com>
2024-06-26 16:38:21 +0700

    feat(view products): update view product feature for customer

--- services/ProductServices.js
+++ services/ProductServices.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const path = require("path");
 
 const connection = require("../services/DBServices");
+const { checkCategoryExists } = require("../services/CategoryServices");
 const {
 	checkNameValid,
 	checkPriceValid,
 	checkQuantityValid,
 	checkDesValid,
 	checkCategoryIdsValid,
+	processCategories,
 } = require("../utils/product_utils");
 const { UPLOADS_BASE_PATH } = require("../utils/constants");
 const { serverProductImagePaths } = require("../utils/file_utils");
@@ -129,7 +131,56 @@ async function addNewProduct(
 	}
 }
 
-async function getProductsForCustomer() {
+/**
+ * This function retrieves products from DB and return to customer 
+ * @param {number} page - THis is the current page number to get data
+ * @param {number} limit - The maximum number of products shown in each page
+ * @param {string} sortBy - The column name to be sorted
+ * @param {string} sortOrder - The sort order 
+ * @param {number | string} categoryId - The id of category which is filtered
+ * @returns The list of products and along with additional information
+ */
+async function getProductsForCustomer(
+	page = 0,
+	limit = 15,
+	sortBy,
+	sortOrder,
+	categoryId
+) {
+	/* 
+	* Count the offset to find where to start returning data
+	* -> Check the sort order
+	* -> Check if the column will be sorted is allowed 
+	*/
+	const offset = (page - 1) * limit;
+	sortOrder = sortOrder.toUpperCase() === "DESC" ? "DESC" : "ASC";
+	const allowedSortColumns = ["name", "price", "like_count", "create_at",];
+	if (!allowedSortColumns.includes(sortBy))
+		sortBy = "create_at";
+	try {
+		if (!categoryId) {
+			return await getProductsForCustomerNoFilter(limit, sortBy, sortOrder, offset);
+		}
+		return await getProductsForCustomerWithFilter(limit, sortBy, sortOrder, categoryId, offset);
+	} catch (error) {
+		return {
+			success: true,
+			message: "Get products failed!",
+			error: error.message,
+		};
+	}
+}
+
+/**The SQL query below says "return only 10 records, start on record 16 (OFFSET 15)":order
+ * @param {number} offset - Where the records start be return
+ * @returns The product data retrieved from DB and total products, pages will have
+ */
+async function getProductsForCustomerNoFilter(
+	limit,
+	sortBy,
+	sortOrder,
+	offset
+) {
 	try {
 		let [queryResults] = await connection.query(
 			`SELECT 
@@ -139,33 +190,43 @@ async function getProductsForCustomer() {
 				quantity, 
 				like_count, 
 				dislike_count, 
-				GROUP_CONCAT(CONCAT(c.id, ':', c.name) SEPARATOR ', ') as categories,
+				GROUP_CONCAT(CONCAT(c.id, ':', c.name) SEPARATOR ',') as categories,
 				image_path
-			FROM 
-				products AS p
-			INNER JOIN
-				product_categories AS pc ON pc.product_id = p.id
-			INNER JOIN
-				categories AS c ON pc.category_id = c.id
-			WHERE 
-				p.is_active = 1
-			GROUP BY
-				p.id`
+			FROM products AS p
+			INNER JOIN product_categories AS pc ON pc.product_id = p.id
+			INNER JOIN categories AS c ON pc.category_id = c.id
+			WHERE p.is_active = 1
+			GROUP BY p.id
+			ORDER BY p.${sortBy} ${sortOrder}
+			LIMIT ?
+			OFFSET ?`,
+			[limit, offset]
 		);
-		if (!queryResults)
-			return {
-				success: false,
-				message: "No products found!",
-			};
-		queryResults = serverProductImagePaths(queryResults)
+		if (!queryResults) return {
+			success: false,
+			message: "No products found!",
+			totalItems: 0,
+			totalPages: 0
+		};
 
+		const [countResults] = await connection.query(
+			`SELECT COUNT(*) AS count 
+			FROM products
+			WHERE is_active = 1`
+		);
+		const totalItems = countResults[0].count;
+		const totalPages = Math.ceil(totalItems / limit);
+
+		queryResults = serverProductImagePaths(queryResults);
+		queryResults = processCategories(queryResults);
 		return {
 			success: true,
 			message: `Found: ${queryResults.length} products`,
+			total_items: totalItems,
+			total_pages: totalPages,
 			data: queryResults,
 		};
 	} catch (error) {
-		console.log(error);
 		return {
 			success: true,
 			message: "Get products failed!",
@@ -174,6 +235,71 @@ async function getProductsForCustomer() {
 	}
 }
 
+async function getProductsForCustomerWithFilter(
+	limit,
+	sortBy,
+	sortOrder,
+	categoryId,
+	offset
+) {
+	try {
+		// * Check if category is available for filtering products
+		const isCategoryAvailable = await checkCategoryExists(categoryId);
+		if (!isCategoryAvailable.success) return isCategoryAvailable;
+		let [queryResults] = await connection.query(
+			`SELECT 
+				p.id,
+				p.name, 
+				price, 
+				quantity, 
+				like_count, 
+				dislike_count, 
+				GROUP_CONCAT(c.id, ':', c.name) as categories,
+				image_path
+			FROM products AS p
+			INNER JOIN product_categories AS pc ON pc.product_id = p.id
+			INNER JOIN categories AS c ON pc.category_id = c.id
+			WHERE p.is_active = 1 AND c.id = ?
+			GROUP BY p.id
+			ORDER BY p.${sortBy} ${sortOrder}
+			LIMIT ?
+			OFFSET ?`,
+			[categoryId, limit, offset]
+		);
+		if (queryResults.length === 0) return {
+			success: false,
+			message: "No products found!",
+			totalItems: 0,
+			totalPages: 0
+		};
+
+		const [countResults] = await connection.query(
+			`SELECT COUNT(*) AS count 
+			FROM products as p
+			INNER JOIN product_categories AS pc ON pc.product_id = p.id
+			WHERE p.is_active = 1 AND pc.category_id = ?`,
+			[categoryId]
+		);
+		const totalItems = countResults[0].count;
+		const totalPages = Math.ceil(totalItems / limit);
+
+		queryResults = serverProductImagePaths(queryResults);
+		queryResults = processCategories(queryResults);
+		return {
+			success: true,
+			message: `Found: ${queryResults.length} products`,
+			total_items: totalItems,
+			total_pages: totalPages,
+			data: queryResults,
+		};
+	} catch (error) {
+		return {
+			success: true,
+			message: "Get products failed!",
+			error: error.message,
+		};
+	}
+}
 module.exports = {
 	validateNewProductInputs,
 	addNewProduct,
